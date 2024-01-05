c5ed0e98cbb4866854f828f9882ec8f3eacb0627
yikanghee <yikanghee2@naver.com>
2024-01-05 15:27:37 +0900

    :tada: :

--- demo/src/main/java/com/demo/DemoApplication.java
+++ demo/src/main/java/com/demo/DemoApplication.java
@@ -2,8 +2,10 @@ package com.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DemoApplication {
     // dev
     public static void main(String[] args) {
